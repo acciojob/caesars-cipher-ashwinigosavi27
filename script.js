456e0978f72dd9743898ade3433fcb6113b4b3e4
Ashwini Gosavi <120784052+ashwinigosavi27@users.noreply.github.com>
2023-05-24 15:40:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 // Your Script here.
+function rot13(encodedString) {
+  // Function to decode a single character using ROT13 algorithm
+  function decodeCharacter(char) {
+    const charCode = char.charCodeAt(0);
+    if (charCode >= 65 && charCode <= 90) {
+      // Decode uppercase letters
+      return String.fromCharCode(((charCode - 65 + 13) % 26) + 65);
+    }
+    // Return non-alphabetic characters as is
+    return char;
+  }
+
+  // Split the encoded string into an array of characters
+  const encodedArray = encodedString.split("");
+
+  // Decode each character in the array
+  const decodedArray = encodedArray.map(decodeCharacter);
+
+  // Join the decoded characters back into a string
+  const decodedString = decodedArray.join("");
+
+  return decodedString;
+}
+
+// Example usage:
+const encodedString = "EBG13 rknzcyr.";
+const decodedString = rot13(encodedString);
+console.log(decodedString); // Output: ROT13 example.
 
 const lookup = {
   'A': 'N','B': 'O','C': 'P','D': 'Q',
